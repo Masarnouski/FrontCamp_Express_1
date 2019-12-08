056ddbd29e22c36a710bcebea5eb9c85bbcbfc64
Yauheni Masarnouski <jeniamasarn94@gmail.com>
2019-12-08 16:30:37 +0300

    rewrite all endpoints to use mongoDB

--- src/news-router.js
+++ src/news-router.js
@@ -36,7 +36,17 @@ router.get('/', async function (req, res, next) {
 });
 
 
-router.get('/:id', function (req, res, next) {
+router.get('/:id', async function (req, res, next) {
+    const articlesModel = mongoConfig.models.Article;
+    try {
+        var article = await articlesModel.findOne({ id: req.params.id });
+        if (article == null) {
+            throw new HttpError(`Article with id: ${req.params.id} not found`, 404)
+        }
+        console.log(article);
+        res.status(200).send(article);
+    }
+    catch (error) { next(error) }
     // readFileAsync(pathToJson).then((data) => {
     //     let news = JSON.parse(data);
     //     let article = news.articles.filter((article => article.id == req.params.id));
@@ -46,11 +56,11 @@ router.get('/:id', function (req, res, next) {
 });
 
 router.post('/', async function (req, res, next) {
-    const newArticle = new mongoConfig.models.Article(req.body);
-    console.log(newArticle);
+    const articlesModel = new mongoConfig.models.Article(req.body);
+    console.log(articlesModel);
     try {
-        await newArticle.save();
-        res.status(200).send();
+        var newArticle = await articlesModel.save();
+        res.status(200).send(newArticle);
     }
     catch (error) {
         next(error)
@@ -70,39 +80,62 @@ router.post('/', async function (req, res, next) {
 
 
 
-router.put('/:id', function (req, res, next) {
-    readFileAsync(pathToJson).then((data) => {
-        let news = JSON.parse(data);
-        let index = news.articles.findIndex((article => article.id == req.params.id));
-        if (index == -1)
-            throw new HttpError('Not found', 404)
-        news.articles[index] = req.body
-        var stringData = JSON.stringify(news);
-        return writeFileAsync(pathToJson, stringData)
-    })
-        .then(() => {
-            console.log('object updated')
-            res.status(200).send('Changed');
-        })
-        .catch(next);
+router.put('/:id', async function (req, res, next) {
+    try {
+        let conditions = { id: req.params.id }
+        var article = await mongoConfig.models.Article.findOneAndUpdate(conditions, req.body);
+        if (article == null) {
+            throw new HttpError(`Article with id: ${req.params.id} not found`, 404)
+        }
+        res.status(200).send(article);
+    }
+    catch (error) {
+        next(error)
+    }
+    // readFileAsync(pathToJson).then((data) => {
+    //     let news = JSON.parse(data);
+    //     let index = news.articles.findIndex((article => article.id == req.params.id));
+    //     if (index == -1)
+    //         throw new HttpError('Not found', 404)
+    //     news.articles[index] = req.body
+    //     var stringData = JSON.stringify(news);
+    //     return writeFileAsync(pathToJson, stringData)
+    // })
+    //     .then(() => {
+    //         console.log('object updated')
+    //         res.status(200).send('Changed');
+    //     })
+    //     .catch(next);
 })
 
 
-router.delete('/:id', function (req, res, next) {
-    readFileAsync(pathToJson).then((data) => {
-        let news = JSON.parse(data);
-        let index = news.articles.findIndex(article => article.id == req.params.id);
-        if (index == -1)
-            throw new HttpError('Not found', 404)
-        news.articles.splice(index, 1);
-        var stringData = JSON.stringify(news);
-        return writeFileAsync(pathToJson, stringData)
-    })
-        .then(() => {
-            console.log('object updated')
-            res.status(200).send('Deleted');
-        })
-        .catch(next);
+router.delete('/:id', async function (req, res, next) {
+    try {
+        var article = await mongoConfig.models.Article.deleteOne({ id:req.params.id });
+        console.log(article);
+        if (article.deletedCount == 0) {
+            throw new HttpError(`Article with id: ${req.params.id} not found`, 404)
+        }
+        res.status(200).send(`Object with id: ${req.params.id} successfully deleted`);
+    }
+    catch (error) {
+        next(error)
+    }
+
+    // readFileAsync(pathToJson).then((data) => {
+    //     let news = JSON.parse(data);
+    //     let index = news.articles.findIndex(article => article.id == req.params.id);
+    //     if (index == -1)
+    //         throw new HttpError('Not found', 404)
+    //     news.articles.splice(index, 1);
+    //     var stringData = JSON.stringify(news);
+    //     return writeFileAsync(pathToJson, stringData)
+    // })
+    //     .then(() => {
+    //         console.log('object updated')
+    //         res.status(200).send('Deleted');
+    //     })
+    //     .catch(next);
 })
 
 
